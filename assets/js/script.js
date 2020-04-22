12803a8ef7e2b3e523123b24d01951d797dcf4df
sadityaprdxn <adityas@prdxn.com>
2020-04-22 13:44:22 +0530

    fetching userdata scripting added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.onload = function () {
+
+  const textarea = document.querySelector("textarea");
+  const url = 'https://raw.githubusercontent.com/saniketprdxn/JsonData/master/Users.json';
+  let Userdata = null;
+
+  fetch(url).then(function(response){
+    return response.json();
+  }).then(function(data){
+    Userdata = data;
+    textarea.readOnly = true;
+    textarea.innerHTML = JSON.stringify(data, null, 1);
+  }).catch(function( error){
+    console.log("something went wrong and the error is " + error)
+  })
+}
